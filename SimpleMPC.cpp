ac094bbf4509d3618ee73fbe9c8d9c209c128793
Ernst Lübeck iMac <marian.forster@me.com>
2019-01-20 19:14:26 +0100

    Dynamic NP working
    all matrices are now calculated in constructor

--- SimpleMPC.cpp
+++ SimpleMPC.cpp
@@ -6,10 +6,33 @@ SimpleMpc::SimpleMpc()
     Serial.println("MPC Constructor:");
     
     calcF((float*)A, (float*)C, NX, NY, NP, (float*)F);
-    MatrixPrint((float*)F, NP, NX);
+    //MatrixPrint((float*)F, NP, NX);
     
     calcPhi((float*) A, (float*) B, (float*) C, NX, NY, NU, NP, (float*) Phi);
-    MatrixPrint((float*)Phi, NP, NP);
+    //MatrixPrint((float*)Phi, NP, NP);
+    
+    float PhiT[NP][NP];
+    MatrixTranspose((float*) Phi, NP, NP, (float*) PhiT);
+    
+    // H = PhiT*Phi + Ru;
+    float Temp[NP][NP];
+    MatrixMultiply((float*)PhiT, (float*)Phi, NP, NP, NP, (float*)Temp);
+    //MatrixPrint((float*)Temp, NP, NP);
+    
+    float Ru[NP][NP] = {{0}};
+    for(int i=0; i<NP; i++)
+    {
+        Ru[i][i] = ru;
+    }
+    
+    float H[NP][NP];
+    MatrixAdd((float*)Temp, (float*)Ru, NP, NP, (float*)H);
+    
+    MatrixInvert((float*) H, NP); // Hinv
+    
+    MatrixMultiply((float*)H, (float*)PhiT, NP, NP, NP, (float*)Hinv_PhiT);
+    
+    MatrixPrint((float*)Hinv_PhiT, NP, NP);
     
 }
 
@@ -255,6 +278,107 @@ void SimpleMpc::MatrixCopy(float* A, int n, int m, float* B)
 		}
 }
 
+void SimpleMpc::MatrixTranspose(float* A, int m, int n, float* C)
+{
+    // A = input matrix (m x n)
+    // m = number of rows in A
+    // n = number of columns in A
+    // C = output matrix = the transpose of A (n x m)
+    int i, j;
+    for (i = 0; i < m; i++)
+        for(j = 0; j < n; j++)
+            C[m * j + i] = A[n * i + j];
+}
+
+//Matrix Inversion Routine
+// * This function inverts a matrix based on the Gauss Jordan method.
+// * Specifically, it uses partial pivoting to improve numeric stability.
+// * The algorithm is drawn from those presented in
+//     NUMERICAL RECIPES: The Art of Scientific Computing.
+// * The function returns 1 on success, 0 on failure.
+// * NOTE: The argument is ALSO the result matrix, meaning the input matrix is REPLACED
+int SimpleMpc::MatrixInvert(float* A, int n)
+{
+    // A = input matrix AND result matrix
+    // n = number of rows = number of columns in A (n x n)
+    int pivrow = 0;        // keeps track of current pivot row
+    int k, i, j;        // k: overall index along diagonal; i: row index; j: col index
+    int pivrows[n]; // keeps track of rows swaps to undo at end
+    float tmp;        // used for finding max value and making column swaps
+    
+    for (k = 0; k < n; k++)
+    {
+        // find pivot row, the row with biggest entry in current column
+        tmp = 0;
+        for (i = k; i < n; i++)
+        {
+            if (abs(A[i * n + k]) >= tmp)    // 'Avoid using other functions inside abs()?'
+            {
+                tmp = abs(A[i * n + k]);
+                pivrow = i;
+            }
+        }
+        
+        // check for singular matrix
+        if (A[pivrow * n + k] == 0.0f)
+        {
+            Serial.println("Inversion failed due to singular matrix");
+            return 0;
+        }
+        
+        // Execute pivot (row swap) if needed
+        if (pivrow != k)
+        {
+            // swap row k with pivrow
+            for (j = 0; j < n; j++)
+            {
+                tmp = A[k * n + j];
+                A[k * n + j] = A[pivrow * n + j];
+                A[pivrow * n + j] = tmp;
+            }
+        }
+        pivrows[k] = pivrow;    // record row swap (even if no swap happened)
+        
+        tmp = 1.0f / A[k * n + k];    // invert pivot element
+        A[k * n + k] = 1.0f;        // This element of input matrix becomes result matrix
+        
+        // Perform row reduction (divide every element by pivot)
+        for (j = 0; j < n; j++)
+        {
+            A[k * n + j] = A[k * n + j] * tmp;
+        }
+        
+        // Now eliminate all other entries in this column
+        for (i = 0; i < n; i++)
+        {
+            if (i != k)
+            {
+                tmp = A[i * n + k];
+                A[i * n + k] = 0.0f; // The other place where in matrix becomes result mat
+                for (j = 0; j < n; j++)
+                {
+                    A[i * n + j] = A[i * n + j] - A[k * n + j] * tmp;
+                }
+            }
+        }
+    }
+    
+    // Done, now need to undo pivot row swaps by doing column swaps in reverse order
+    for (k = n - 1; k >= 0; k--)
+    {
+        if (pivrows[k] != k)
+        {
+            for (i = 0; i < n; i++)
+            {
+                tmp = A[i * n + k];
+                A[i * n + k] = A[i * n + pivrows[k]];
+                A[i * n + pivrows[k]] = tmp;
+            }
+        }
+    }
+    return 1;
+}
+
 void SimpleMpc::calcF(float* A, float* C, int nx, int ny, int np, float* F)
 {
     float Temp[nx][ny];
@@ -296,8 +420,7 @@ void SimpleMpc::calcPhi(float* A, float* B, float* C, int nx, int ny, int nu, in
     MatrixMultiply((float*)C, (float*)B, ny, nx, nu, (float*)Temp3);
     
     firstColPhi[0] = Temp3[0];
-    
-    
+
     for(int i=1; i<np; i++)
     {
         // Temp1 = Temp1 * A;
@@ -313,7 +436,7 @@ void SimpleMpc::calcPhi(float* A, float* B, float* C, int nx, int ny, int nu, in
         firstColPhi[i] = Temp3[0];
     }
     
-    MatrixPrint((float*)firstColPhi, NP, 1);
+    //MatrixPrint((float*)firstColPhi, NP, 1);
     
     int offs = 0;
     for(int ii=0; ii<np*nu; ii++)
@@ -329,7 +452,7 @@ void SimpleMpc::calcPhi(float* A, float* B, float* C, int nx, int ny, int nu, in
     }
     
     //delay(100);
-    MatrixPrint((float*)Phi, np, np);
+    //MatrixPrint((float*)Phi, np, np);
 
 }
 
